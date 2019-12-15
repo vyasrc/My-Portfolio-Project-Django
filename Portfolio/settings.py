f372d0feafcaa1830ab644f466fa0ba8e08feb05
Vyas Cholayil <vyas.cholayil@yahoo.com>
2019-12-15 18:45:12 -0500

    Update settings.py

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'portfoliodb',
         'USER': 'postgres',
-        'PASSWORD': 'vicchu1812',
+        'PASSWORD': 'django123',
         'HOST': 'localhost',
         'PORT': '5432',
     }
